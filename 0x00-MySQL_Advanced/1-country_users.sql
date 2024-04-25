3151fcc4fb84f746bef44ca95dca6e0ddd01c196
root <bereketzeselassie@gmail.com>
2024-04-25 08:38:20 +0000

    coutry

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- in and not out
+CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(225),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT'US'
+);
