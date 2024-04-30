af18d15554f61e7b750fddac108253edae41119a
bakiizese <bereketzeselassie>
2024-04-30 13:42:00 -0700

    fied

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
-
+''' list all '''
 
 def list_all(mongo_collection):
-    """ List all documents in Python """
+    ''' all '''
     documents = mongo_collection.find()
 
     if documents.count() == 0:
