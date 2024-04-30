7e4325f93aae3c0ce5de51cf98d491dc525bd0e0
bakiizese <bereketzeselassie>
2024-04-30 14:34:51 -0700

    finds

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''find'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''topics find'''
+    calls = mango_collection.find({"topics": topic})
+    return list(calls)
