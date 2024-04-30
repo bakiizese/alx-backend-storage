88729f239dbfd693cad1bb7eb2355c9cbe94ac48
bakiizese <bereketzeselassie>
2024-04-30 14:49:24 -0700

    fix

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from pymongo import MongoClient
 ''' find '''
 
 
 def schools_by_topic(mongo_collection, topic):
     ''' topics find '''
-    documents = mango_collection.find({"topics": topic})
-    return list(documents)
+    documents = mango_collection.find({"topics": {'$elemMatch': {'$eq': topic}}})
+    return [d for d in documents]
