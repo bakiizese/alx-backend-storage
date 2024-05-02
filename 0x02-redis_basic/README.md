4085b9813965b27baee28858d5aae9b3dafac5a0
bakiizese <bereketzeselassie@gmail.com>
2024-05-02 00:12:41 +0000

    reddis

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,1 @@
+redis db basices in py
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''redis 00'''
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        ''' connect '''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, float, int]) -> str:
+        ''' returns str key '''
+        k: str = str(uuid.uuid4()) 
+        self._redis.set(k, data)
+        return k
