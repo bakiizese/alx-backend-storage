dfb25933d11a80a370965defaaa694ff4c5567ae
bakiizese <bereketzeselassie@gmail.com>
2024-05-02 00:45:36 +0000

    01

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,3 +16,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        ''' convo '''
+        val = self._redis.get(key)
+        if fn:
+            val = fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        ''' return str '''
+        val = self._redis.get(key)
+        return val.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        ''' int return '''
+        val = self.redis.get(key)
+        try:
+            val = int(value.decode('utf-8'))
+        except Exception:
+            val = 0
+        return vl
