6d1826cf070802420c24eb61aa95d7232532eb41
bakiizese <bereketzeselassie@gmail.com>
2024-05-02 01:38:51 +0000

    try;

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
-''' web '''
+""" Redis Module """
+
+from functools import wraps
 import redis
 import requests
-from functools import wraps
 from typing import Callable
 
-rediss = redis.Redis()
+redis_ = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    ''' counts '''
+    """ Decortator for counting """
     @wraps(method)
-    def wrapper(url):
-        ''' wrap '''
-        rediss.incr(f"count:{url}")
-        cached = rediss.get(f"cached:{url}")
-        if cached:
-            return cached.decode('utf-8')
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
         html = method(url)
-        rediss.setex(f"cached:{url}", 10, html)
+        redis_.setex(f"cached:{url}", 10, html)
         return html
+
     return wrapper
 
 
 @count_requests
 def get_page(url: str) -> str:
-    ''' return count '''
-    requestss = requests.get(url)
-    return requestss.text
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
